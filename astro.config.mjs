219dab281e3f5ae686af8192455f6d0c028a9350
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-05-29 17:42:44 -0400

    adding tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import astroIcon from 'astro-icon';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  server:{port:1234},
+  server: {
+    port: 1234
+  },
   renderers: [react()],
-  integrations: [react(),  astroIcon()]
+  integrations: [react(), astroIcon(), tailwind()]
 });
