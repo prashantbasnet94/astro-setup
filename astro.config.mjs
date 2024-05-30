19092f8ebfc06114b0cd5e9c82fa79e653a4c35b
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-05-30 11:24:08 -0400

    adding fonts and  base layout for header and metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from '@astrojs/react';
 import astroIcon from 'astro-icon';
-
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -9,6 +9,11 @@ export default defineConfig({
   server: {
     port: 1234
   },
-  renderers: [react()],
-  integrations: [react(), astroIcon(), tailwind()]
+  renderers: [ react() ],
+  integrations: [ react(), astroIcon(),
+    tailwind({applyBaseStyles: false}),
+    sitemap() ],
+  output: "hybrid",
+  site: "https://unisala.com",
 });
+
--- src/components/main-navigation-menu.tsx
+++ src/components/main-navigation-menu.tsx
@@ -49,16 +49,7 @@ export function MainNavigationMenu() {
           </NavigationMenuContent>
         </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>{pages.title}</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {pages.items?.map((page) => (
-                <ListItem key={page.title} {...page} />
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
+
 
         <NavigationMenuItem>
           <NavigationMenuTrigger>{examples.title}</NavigationMenuTrigger>
